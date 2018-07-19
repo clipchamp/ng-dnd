c2d10df1f2b30f8e90e9f8ef8bc33947f9c0cd33
Timo Hillmann <timo@clipchamp.com>
2018-07-19 17:37:49 +1000

    Adjusted event interface

--- projects/ng-dnd/src/lib/backends/drag-backend-event.ts
+++ projects/ng-dnd/src/lib/backends/drag-backend-event.ts
@@ -1,12 +1,14 @@
 import { DragBackendEventType } from './drag-backend-event-type';
 import { Coordinates } from './offset';
 import { DragSource } from '../drag-source.directive';
+import { DropTarget } from '../drop-target.directive';
 
 export interface DragBackendEvent {
   type: DragBackendEventType;
   sourceId?: string;
   source?: DragSource;
   targetId?: string;
+  target?: DropTarget;
   item?: any;
   clientOffset: Coordinates;
   sourceOffset?: any;
