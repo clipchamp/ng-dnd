dfc418acf97cbf8c70439c2e9e761f533a500fc9
Timo Hillmann <timo@clipchamp.com>
2018-07-25 08:46:08 +1000

    Updated to 0.0.5

--- projects/ng-dnd/src/public_api.ts
+++ projects/ng-dnd/src/public_api.ts
@@ -1,5 +1,5 @@
 /*
- * Public API Surface of drag-n-drop
+ * Public API Surface of ng-dnd
  */
 
 export * from './lib/backends/drag-backend-event-type';
@@ -8,7 +8,12 @@ export * from './lib/backends/drag-backend-factory';
 export * from './lib/backends/drag-backend';
 export * from './lib/backends/html5-drag-backend';
 export * from './lib/drag-source/drag-source.directive';
+export * from './lib/drag-source/drag-source-dragging.directive';
 export * from './lib/drop-target/drop-target.directive';
+export * from './lib/drop-target/drop-target-over.directive';
+export * from './lib/drop-target/drop-target-dragging.directive';
+export * from './lib/drop-target/if-over.directive';
+export * from './lib/drop-target/if-dragging.directive';
 export * from './lib/drag-registry';
 export * from './lib/drag-monitor';
 export * from './lib/drag-dispatcher.service';
